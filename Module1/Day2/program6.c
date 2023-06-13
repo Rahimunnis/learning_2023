1bda45e61c8acdb7e27867075f11295e1c76dedd
Rahimunnis <rahimunny2001@gmail.com>
2023-06-13 23:46:47 +0530

    LEVEL 2

--- Module1/Day2/program6.c
+++ Module1/Day2/program6.c
@@ -13,22 +13,10 @@ int main(){
     for(i=0;i<n;i++){
         printf("%d\t",a[i]);
     }
-    if(n%3==0){
-        for(i=0;i<n;i=i+3){
-        
-            temp=a[i];
-            a[i]=a[i+2]; //swapping 
-            a[i+2]=temp;
-        }
-    }
-    else{
-        for(i=0;i<=n-3;i=i+3){
-        
-            temp=a[i];
-            a[i]=a[i+2]; //swapping
-            a[i+2]=temp;
-        }
-
+    for(i=0;i<=n-3;i=i+3){
+        temp=a[i];
+        a[i]=a[i+2]; //swapping of array elements
+        a[i+2]=temp;
     }
     printf("\nAfter swapping:\n");
     for(i=0;i<n;i++){
---
+++ Module1/Day2/program7.c
@@ -0,0 +1,50 @@
+// program 2 level 2
+#include <stdio.h>
+int check_similarity();
+int main(){
+    int result;
+    result=check_similarity();
+    printf("\nchecking given arrays are same or not!");
+    printf("\nif true it will print 1 else it will print 0");
+    printf("\nresult is %d",result);
+    return 0;
+}
+int check_similarity() {
+    int n;
+    printf("Enter number:");
+    scanf("%d",&n);
+    int a[n];
+    int b[n];
+    printf("Enter elements of array1:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Enter elements of array2:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&b[i]);
+    }
+    printf("\nElements of array1:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",a[i]);
+    }
+    printf("\nElements of array2:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",b[i]);
+    }
+    int count=0;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(a[i]==b[j]){
+                count++;
+            }
+        }
+    }
+    if(count==3){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+    
+}
