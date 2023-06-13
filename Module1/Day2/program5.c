c4adbef729d4593d4e023ffddfdfec9b11c818e8
Rahimunnis <rahimunny2001@gmail.com>
2023-06-13 19:49:03 +0530

    LEVEL 1

--- Module1/Day2/program5.c
+++ Module1/Day2/program5.c
@@ -1,15 +1,14 @@
-//program 4 solution
+//program 5 soultion
 #include <stdio.h>
-
 int main() {
-    int a[3]={0x01,0xF4,0x10001};
+    int a[3]={1,4,5};
     int cnt=0;
     int i;
     for(i=0;i<3;i++){
         while(a[i]>0){
             a[i]=a[i]&(a[i]-1);
             cnt++;
-        }    
+        }
     }
     printf("\ncnt is %d",cnt);
     return 0;
