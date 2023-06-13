2bb62bce2db9c32862f6217a3efa339f548b2b80
Rahimunnis <rahimunny2001@gmail.com>
2023-06-13 22:13:44 +0530

    LEVEL 2

---
+++ Module1/Day2/program6.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main(){
+    int i,temp,n;
+    printf("Enter array size:");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter array elemets:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("\nBefore swapping:\n");
+    for(i=0;i<n;i++){
+        printf("%d\t",a[i]);
+    }
+    if(n%3==0){
+        for(i=0;i<n;i=i+3){
+        
+            temp=a[i];
+            a[i]=a[i+2]; //swapping 
+            a[i+2]=temp;
+        }
+    }
+    else{
+        for(i=0;i<=n/2;i=i+3){
+        
+            temp=a[i];
+            a[i]=a[i+2]; //swapping
+            a[i+2]=temp;
+        }
+
+    }
+    printf("\nAfter swapping:\n");
+    for(i=0;i<n;i++){
+        printf("%d\t",a[i]);
+    }
+    return 0;
+}
