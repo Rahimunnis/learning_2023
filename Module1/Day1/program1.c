f78b81baef5a61262d930b01542690918c4c1b30
Rahimunnis <rahimunny2001@gmail.com>
2023-06-13 14:02:43 +0530

    level 1 question 3

--- Module1/Day1/program1.c
+++ Module1/Day1/program1.c
@@ -1,21 +1,32 @@
 #include <stdio.h>
-void printbignum(int a,int b);
+void printbignum1(int a,int b);
+void printbignum2(int a,int b);
 int main() {
     int a,b;
     printf("Enter a value:");
     scanf("%d",&a);
     printf("Enter b value:");
     scanf("%d",&b);
-    printbignum(a,b);
+    //using if elseif..statement
+    printbignum1(a,b);
+    //using ternary operator 
+    printbignum2(a,b);
     
 
     return 0;
 }
-void printbignum(int a,int b){
+void printbignum1(int a,int b){
+    printf("\n Using if elseif block!");
     if(a>b){
-        printf("a is big where a=%d,b=%d",a,b);
+        printf("\na is big where a=%d,b=%d",a,b);
     }
     else{
-        printf("b is big where b=%d,a=%d",b,a);
+        printf("\nb is big where b=%d,a=%d",b,a);
     }
+}
+void printbignum2(int a,int b){
+    printf("\nUsing ternary operator! ");
+    printf("\na value is %d",a);
+    printf("\nb value is %d",b);
+    printf("\nBiggest value is %d",(a>b)?a:b);
 }
---
+++ Module1/Day1/program3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+//#include<strings.h>
+int main() {
+    int roll;
+    char name[20];
+    int marks[3];
+    int total;
+    float percentage;
+    printf("Enter roll number:");
+    scanf("%d",&roll);
+    printf("Enter name:");
+    scanf("%s",name);
+    printf("Enter physics marks:");
+    scanf("%d",&marks[0]);
+    printf("Enter maths marks:");
+    scanf("%d",&marks[1]);
+    printf("Enter chemistry marks:");
+    scanf("%d",&marks[2]);
+    total=marks[0]+marks[1]+marks[2];
+    printf("\nroll number:%d",roll);
+    printf("\nname:%s",name);
+    printf("\nTotal marks=%d",total);
+    percentage=(float)total/3;
+    printf("\npercentage =%f %%",percentage);
+    
+
+    return 0;
+}
