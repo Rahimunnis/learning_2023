4cae3fa62b6d9cfdb6d9313c2f077c4dac699dff
Rahimunnis <rahimunny2001@gmail.com>
2023-06-15 04:42:50 +0530

    LEVEL 1

--- Module1/Day3/program4.c
+++ Module1/Day3/program4.c
@@ -1,4 +1,4 @@
-//program 4 level 1 solution
+//program 4 level 1 solution using array
 #include <stdio.h>
 int main() {
     int a,t,cnt;
@@ -29,6 +29,47 @@ int main() {
             if(arr[j]<s){
                 s=arr[j];
             }
+        }
+        printf("\nsmallest digit in a number=%d",s);
+        printf("\nbiggest digit in a given number=%d",b);
+    }
+    else{
+        printf("\nNot valid number(min 2 digits)!");
+    }
+
+    return 0;
+}
+//program 4 level 1 solution without using array
+#include <stdio.h>
+int main() {
+    int a;
+    printf("\nEnter a number:");
+    scanf("%d",&a);
+    int t=a,cnt=0;
+    int s,b=0,r;
+    while(a>0){
+        a=a/10;
+        cnt++;
+    }
+    a=t;
+    printf("\ngiven number is %d",a);
+    printf("\ncount of digits in a number is %d",cnt);
+    if(cnt>1){
+        while(a>0){
+            r=a%10;
+            if(r>b){
+                b=r;
+            }
+            a=a/10;
+        }
+        a=t;
+        s=b;
+        while(a>0){
+            r=a%10;
+            if(r<s){
+                s=r;
+            }
+            a=a/10;
         }
         printf("\nsmallest digit in a number=%d",s);
         printf("\nbiggest digit in a given number=%d",b);
